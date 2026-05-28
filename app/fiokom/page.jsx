55908be6742ec063908ff918fce1988c09ef0f02
Gál Áron Szili <sziligalaron@gmail.com>
2026-05-28 16:33:31 +0200

    fiók oldal reszponzivitás

--- app/fiokom/page.jsx
+++ app/fiokom/page.jsx
@@ -11,8 +11,8 @@ import { useState } from "react";
 export default function UserDashboard() {
   const [currentMenu, setCurrentMenu] = useState("orders");
   return (
-    <div className="user-dashboard flex flex-row gap-3 w-[90%] md:w-[80%] max-w-[1800px] my-[50px] mx-auto min-h-[100vh] items-start">
-      <div className="user-dashboard-nav flex flex-col gap-3 px-2.5 py-[25px] bg-neutral-900/70 rounded-lg overflow-y-auto w-[250px]">
+    <div className="user-dashboard flex flex-col md:flex-row gap-3 w-[100%] p-[10px] md:w-[80%] max-w-[1800px] my-[50px] mx-auto min-h-[100vh] items-start">
+      <div className="user-dashboard-nav w-[100%] grid grid-cols-3 md:w-[250px] md:flex md:flex-col gap-3 px-2.5 py-[25px] bg-neutral-900/70 rounded-lg overflow-y-auto ">
         <div
           onClick={() => signOut()}
           className="flex flex-row w-full relative p-2.5 rounded-xl bg-neutral-700 border border-neutral-800 hover:border-teal-500/50 transition-all gap-3 cursor-pointer "
@@ -36,7 +36,7 @@ export default function UserDashboard() {
         </div>
       </div>
 
-      <div className="user-dashboard-main w-[80%] p-2 bg-neutral-900/70 rounded-lg ">
+      <div className="user-dashboard-main w-[100%] md:w-[80%] p-2 bg-neutral-900/70 rounded-lg ">
         {currentMenu === "orders" ? <UserOrders /> : <UserData />}
       </div>
     </div>
