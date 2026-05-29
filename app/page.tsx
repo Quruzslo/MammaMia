fa9ac1cf3d936834d250476163a6b8e42c30a4df
Gál Áron Szili <sziligalaron@gmail.com>
2026-05-29 16:52:12 +0200

    Kezdő page szélesítése telón

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/szekciok/Hero";
 import Menu from "@/components/szekciok/Menu";
 export default function App() {
   return (
-    <section className="max-w-[1800px] w-[80%] mx-auto h-full justify-center">
+    <section className="max-w-[1800px] w-[100%] md:w-[80%] mx-auto h-full justify-center">
       <Hero></Hero>
       <Menu></Menu>
     </section>
