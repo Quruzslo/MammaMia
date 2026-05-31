68ab20a6a40b5cb1d3cfca7db4185822b4e81f32
Gál Áron Szili <sziligalaron@gmail.com>
2026-05-31 14:20:51 +0200

    Fiók navok igazítása

--- app/fiokom/page.jsx
+++ app/fiokom/page.jsx
@@ -12,7 +12,7 @@ export default function UserDashboard() {
   const [currentMenu, setCurrentMenu] = useState("orders");
   return (
     <div className="user-dashboard flex flex-col md:flex-row gap-3 w-[100%] p-[10px] md:w-[80%] max-w-[1800px] my-[50px] mx-auto min-h-[100vh] items-start">
-      <div className="user-dashboard-nav w-[100%] grid grid-cols-3 md:w-[250px] md:flex md:flex-col gap-2 px-[5px] py-[25px] bg-neutral-900/70 rounded-lg overflow-y-auto ">
+      <div className="user-dashboard-nav w-[100%] grid grid-cols-1 md:grid-cols-3 md:w-[250px] md:flex md:flex-col gap-2 px-[5px] py-[25px] bg-neutral-900/70 rounded-lg overflow-y-auto ">
         <div
           onClick={() => signOut()}
           className="flex flex-row w-full relative p-[5px] rounded-xl bg-neutral-700 border border-neutral-800 hover:border-teal-500/50 transition-all gap-2 cursor-pointer "
