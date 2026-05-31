4af68171cb87aedca1321744c91bcbae710c8161
Gál Áron Szili <sziligalaron@gmail.com>
2026-05-31 15:43:26 +0200

    body h javítva min-h -ra

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-lvh flex flex-col">
+      <body className="min-h-lvh flex flex-col">
         <UserProvider>
           <CartProvider>
             <Header></Header>
