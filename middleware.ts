67e436e9241ea16b6aa88d8e6fc43204933e85a7
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-01 21:31:02 +0200

    feltoltes oldal is levédve middleware-ben

--- middleware.ts
+++ middleware.ts
@@ -7,21 +7,25 @@ const { auth: middlewareAuth } = NextAuth(authConfig);
 export default middlewareAuth((req) => {
   const isLoggedIn = !!req.auth;
   const nextUrl = req.nextUrl;
+  const pathname = nextUrl.pathname;
 
+  // 1. SIMA FELHASZNÁLÓI OLDALAK VÉDELME
   if (
     !isLoggedIn &&
-    (nextUrl.pathname.startsWith("/fiokom") ||
-      nextUrl.pathname.startsWith("/rendelesek"))
+    (pathname.startsWith("/fiokom") ||
+      pathname.startsWith("/admin/rendelesek") ||
+      pathname.startsWith("/admin/feltoltes"))
   ) {
     const newUrl = new URL("/belepes", nextUrl.origin);
     return Response.redirect(newUrl);
   }
 
-  if (
-    nextUrl.pathname.startsWith("/admin") &&
-    nextUrl.pathname !== "/admin-login"
-  ) {
+  // 2. ADMIN ALOLDALAK VÉDELME (Kivéve maga a login)
+
+  if (pathname.startsWith("/admin") && pathname !== "/admin") {
     const userRole = req.auth?.user?.role;
+
+    // Ha nincs belépve, VAGY belépett, de nem admin a role
     if (!isLoggedIn || userRole !== "admin") {
       const newUrl = new URL("/belepes", nextUrl.origin);
       return Response.redirect(newUrl);
@@ -29,6 +33,7 @@ export default middlewareAuth((req) => {
   }
 });
 
+// 3. A MATCHER LISTÁJA
 export const config = {
-  matcher: ["/fiokom/:path*", "/rendelesek"],
+  matcher: ["/fiokom/:path*", "/rendelesek/:path*", "/admin/:path*"],
 };
