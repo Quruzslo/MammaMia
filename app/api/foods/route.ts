1ff685d06bb176bdc233415879c1795b331df22e
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-03 14:42:21 +0200

    Órafordulón frissülő cache a rendelés blokkolás miatt

--- app/api/foods/route.ts
+++ app/api/foods/route.ts
@@ -2,16 +2,25 @@
 import { NextResponse } from "next/server";
 import client from "@/lib/mongodb";
 
-// cacheljük-------
-export const revalidate = 3600;
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const db = client.db("MammaMia");
     const productsFromDb = await db.collection("foods").find({}).toArray();
 
+    // Kiszámoljuk, hány másodperc van hátra a következő egész óráig
+    const now = new Date();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    // Hátralévő percek és másodpercek átváltása másodpercre
+    const secondsUntilNextHour = (59 - minutes) * 60 + (60 - seconds);
+
     return NextResponse.json(productsFromDb, {
       headers: {
-        "Cache-Control": "s-maxage=3600, stale-while-revalidate=59",
+        // A CDN és a böngésző pontosan a következő óra fordulójáig fogja cache-elni
+        "Cache-Control": `s-maxage=${secondsUntilNextHour}, stale-while-revalidate=59`,
       },
     });
   } catch (err) {
