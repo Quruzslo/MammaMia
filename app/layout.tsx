f0c66fbf609e6ac556d1af6aace7f8a7129c5fb8
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-08 11:37:55 +0200

    toast idő rövidítve

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
             <SideCart></SideCart>
             <ToastContainer
               position="bottom-right"
-              autoClose={1500}
+              autoClose={1000}
               theme="dark"
             />
             {children}
