fb0bc9c9bc205268472db7adb9165022b2059a85
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-10 16:57:11 +0200

    user dashboard nav

--- app/fiokom/page.jsx
+++ app/fiokom/page.jsx
@@ -12,7 +12,7 @@ export default function UserDashboard() {
   const [currentMenu, setCurrentMenu] = useState("orders");
   return (
     <div className="user-dashboard flex flex-col md:flex-row gap-3 w-[100%] bg-neutral-900  min-h-[100vh] items-start px-4 py-4">
-      <div className="user-dashboard-nav w-[100%] sticky top-[75px] rounded-xl border border-neutral-600/80 grid grid-cols-1 md:grid-cols-3 md:w-[250px] md:flex md:flex-col gap-2 px-[5px] py-[25px]  rounded-lg overflow-y-auto ">
+      <div className="user-dashboard-nav w-[100%] md:sticky md:top-[75px] rounded-xl border border-neutral-600/80 grid grid-cols-1 md:grid-cols-3 md:w-[250px] md:flex md:flex-col gap-2 px-[5px] py-[25px]  rounded-lg overflow-y-auto ">
         <div
           onClick={() => signOut()}
           className="flex flex-row w-auto relative py-[5px] px-[20px] rounded-sm bg-neutral-700 border border-neutral-800 hover:border-teal-500/50 transition-all gap-2 cursor-pointer "
