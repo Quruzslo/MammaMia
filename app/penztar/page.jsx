1a1e63e8c6b154ecc2ad12e20ed4be57007341ca
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-12 13:53:50 +0200

    checkout nyilak

--- app/penztar/page.jsx
+++ app/penztar/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 import CheckoutInputs from "./CheckoutInputs";
 import CheckoutOrders from "./CheckoutOrders";
 import { useContext } from "react";
@@ -13,17 +14,53 @@ export default function Checkout() {
   };
   return (
     <>
-      <section className="mt-[50px] align-center justify-center flex  max-w-[1800px] w-[90%] md:w-[80%] mx-auto ">
-        {" "}
-        {formState === "form" ? (
-          <h1 className="text-[30px] rounded-lg shadow uppercase  text-white text-center ">
+      <section className="mt-[50px] align-center justify-center flex  max-w-[1800px] w-[90%] md:w-[80%] mx-auto gap-3 ">
+        {/* Étlap */}
+        <div
+          className={`rounded-l-sm p-[10px] pr-[35px] transition-colors duration-300 
+              bg-white text-black
+          `}
+          style={{
+            clipPath: "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)",
+          }}
+        >
+          <Link
+            className="text-[15px] md:text-[30px] uppercase font-bold text-center tracking-wide"
+            href="/"
+          >
+            Étlap
+          </Link>
+        </div>
+        {/* pénztár NYÍL */}
+        <div
+          className={`rounded-l-sm p-[10px] pr-[35px] transition-colors duration-300 ${
+            formState === "form"
+              ? "bg-teal-600 text-white"
+              : "bg-white text-black "
+          }`}
+          style={{
+            clipPath: "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)",
+          }}
+        >
+          <h2 className="text-[15px] md:text-[30px] uppercase font-bold text-center tracking-wide">
             Pénztár
-          </h1>
-        ) : (
-          <h1 className="text-[30px] rounded-lg shadow uppercase  text-white text-center ">
+          </h2>
+        </div>
+        {/* FIZETÉS NYÍL */}
+        <div
+          className={`rounded-l-sm p-[10px] pr-[35px] transition-colors duration-300 ${
+            formState === "stripe"
+              ? "bg-teal-600 text-white"
+              : "bg-white text-black "
+          }`}
+          style={{
+            clipPath: "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)",
+          }}
+        >
+          <h2 className=" text-[15px] md:text-[30px] uppercase font-bold text-center tracking-wide">
             Fizetés
-          </h1>
-        )}
+          </h2>
+        </div>
       </section>
       <section className="flex flex-col md:flex-row gap-2 max-w-[1800px] w-[90%] md:w-[80%] mx-auto my-[50px]">
         <div className=" w-[100%] md:w-[50%]">
