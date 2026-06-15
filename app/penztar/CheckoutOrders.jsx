551888f2bf2a2ee8586a4f8d6f53f73f7b9f63d8
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-15 16:02:03 +0200

    checkout order javítzva

--- app/penztar/CheckoutOrders.jsx
+++ app/penztar/CheckoutOrders.jsx
@@ -65,7 +65,7 @@ export default function CheckoutOrders({ expiredItem }) {
               <div className="bg-gray-300 px-3 py-2 border-b border-gray-200 flex justify-between items-center">
                 <span className="text-gray-800 font-semibold text-sm uppercase tracking-wide">
                   {nap.dayName}{" "}
-                  {expiredItem ? (
+                  {expiredItem === nap.date ? (
                     <p className="text-red-600 text-bold ml-[10px]">Lejárt</p>
                   ) : null}
                 </span>
