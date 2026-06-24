8c70342596a087c71937fd8b4c9029136419c3f4
Gál Áron Szili <sziligalaron@gmail.com>
2026-06-24 12:28:15 +0200

    body::before bg leceerélve layoutba fix div + optimalizált képre

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from "@/components/szekciok/Footer";
 import ScrollToTop from "@/components/szekciok/scrollToTop";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Image from "next/image"; // Beimportáljuk a Next Image-et
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,21 +42,39 @@ export default function RootLayout({
       data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} h-full antialiased`}
     >
-      <body className="font-nunito flex flex-col">
-        <UserProvider>
-          <CartProvider>
-            <Header></Header>
-            <SideCart></SideCart>
-            <ToastContainer
-              position="bottom-right"
-              autoClose={1000}
-              theme="dark"
-            />
-            {children}
-            <ScrollToTop></ScrollToTop>
-            <Footer></Footer>
-          </CartProvider>
-        </UserProvider>
+      <body className="font-nunito flex flex-col relative min-h-screen text-gray-100 bg-neutral-950">
+        {/* OPTIMALIZÁLT, FIX HÁTTÉRKÉP RÉTEG */}
+        <div className="fixed inset-0 -z-50 pointer-events-none select-none">
+          <div className="absolute inset-0 bg-black/80 z-10" />
+
+          <Image
+            src="/root-bg.jpg"
+            alt="Mamma Mia Háttér"
+            fill
+            sizes="100vw"
+            quality={75}
+            priority
+            className="object-cover object-center"
+          />
+        </div>
+
+        {/* AZ OLDAL TÉNYLEGES TARTALMA */}
+        <div className="relative z-10 flex flex-col min-h-screen">
+          <UserProvider>
+            <CartProvider>
+              <Header></Header>
+              <SideCart></SideCart>
+              <ToastContainer
+                position="bottom-right"
+                autoClose={1000}
+                theme="dark"
+              />
+              {children}
+              <ScrollToTop></ScrollToTop>
+              <Footer></Footer>
+            </CartProvider>
+          </UserProvider>
+        </div>
       </body>
     </html>
   );
